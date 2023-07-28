557d46001d664c25039ecb45c7e964b8dbb60d6d
Aleksandr <drammtv@gmail.com>
2023-07-28 04:24:43 +0300

    Added redis vars to config

--- config.default.py
+++ config.default.py
@@ -1,2 +1,8 @@
-API_TOKEN = None  # api token of telegram bot
-DATABASE_URL = None  # path to database
+API_TOKEN: str = None  # api token of telegram bot
+DATABASE_URL: str = None  # path to database
+
+
+REDIS_HOST: str = None
+REDIS_PORT: int = None
+REDIS_DB: int = None
+REDIS_PASSWORD: str = None
