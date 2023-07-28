6033c3d27fa74b047c7729371632cbeaee9fb77b
Aleksandr <drammtv@gmail.com>
2023-07-29 01:32:11 +0300

    Added postgres info to config, created example env

--- config.default.py
+++ config.default.py
@@ -1,8 +1,21 @@
-API_TOKEN: str = None  # api token of telegram bot
-DATABASE_URL: str = None  # path to database
+import os
 
 
-REDIS_HOST: str = None
-REDIS_PORT: int = None
-REDIS_DB: int = None
-REDIS_PASSWORD: str = None
+API_TOKEN = os.getenv('API_TOKEN', '')
+MY_TELEGRAM_ID = os.getenv('MY_TELEGRAM_ID', 0)
+
+REDIS_HOST: str = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT: int = os.getenv('REDIS_PORT', 6379)
+REDIS_DB: int = os.getenv('REDIS_DB', 0)
+REDIS_PASSWORD: str = os.getenv('REDIS_PASSWORD', '')
+
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'telegram_bot_db')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT', 5432)
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
+
+DATABASE_URL = (
+    f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
+    f'@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+)
