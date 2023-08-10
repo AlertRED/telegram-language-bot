f58d5bd9c9c99918c983487b0e4daacc8bde5454
Aleksandr <drammtv@gmail.com>
2023-08-10 16:53:11 +0300

    Added validation for createing collection

--- bot/handlers/add_item/handlers/add_collection.py
+++ bot/handlers/add_item/handlers/add_collection.py
@@ -23,7 +23,7 @@ async def choose_collection(
     FolderSelectCallback.filter(),
     CreateCollectionStates.choose_place,
 )
-async def collection_choosen(
+async def write_collection_name_callback(
     callback: types.CallbackQuery,
     callback_data: FolderSelectCallback,
     state: FSMContext,
@@ -32,6 +32,13 @@ async def collection_choosen(
         folder_id=callback_data.folder_id,
         folder_name=callback_data.folder_name,
     )
+    await write_collection_name(callback, state)
+
+
+async def write_collection_name(
+    callback: types.CallbackQuery,
+    state: FSMContext,
+) -> None:
     await callback.message.edit_text(text=_('Write set name'))
     await state.set_state(CreateCollectionStates.choose_name)
 
@@ -40,6 +47,24 @@ async def collection_choosen(
 async def create_collection(message: types.Message, state: FSMContext):
     await state.update_data(collection_name=message.text)
     user_data = await state.get_data()
+
+    collection = dao.get_collection(
+        collection_name=user_data['collection_name'],
+        folder_id=user_data['folder_id'],
+    )
+    if collection:
+        await message.answer(
+            text=(
+                'The collection <b><u>{collection_name}</u></b>'
+                ' is already exists in the folder {folder_name}!'
+            ).format(
+                collection_name=user_data['collection_name'],
+                folder_name=user_data['folder_name'],
+            ),
+        )
+        await write_collection_name(message.answer, state)
+        return
+
     dao.create_collection(
         telegram_user_id=message.from_user.id,
         collection_name=user_data['collection_name'],
@@ -48,8 +73,7 @@ async def create_collection(message: types.Message, state: FSMContext):
     await message.answer(
         text=_(
             'Set <b><u>{collection_name}</u></b> '
-            'added into <b><u>{folder_name}</u></b>'
-            ' folder'
+            'added into <b><u>{folder_name}</u></b> folder'
         ).format(
             collection_name=user_data["collection_name"],
             folder_name=user_data["folder_name"] or "Root"
